ff3ce4d391e99f78f4add774d5cfabb415e72cdf
arkapravabanerjee <arkaprava.banerjee@gmail.com>
2020-07-22 02:31:34 +0530

    Updates

---
+++ precache-manifest.1513c1a51191518b5a8adc71c8d8676e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab5de17849621c6b0ecec463a6ba879",
+    "url": "/MyBookstore/index.html"
+  },
+  {
+    "revision": "8f62d0173adf3e7ec849",
+    "url": "/MyBookstore/static/css/main.532ba2c2.chunk.css"
+  },
+  {
+    "revision": "742580132b01ebc3cb9e",
+    "url": "/MyBookstore/static/js/2.aea0e3fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyBookstore/static/js/2.aea0e3fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f62d0173adf3e7ec849",
+    "url": "/MyBookstore/static/js/main.3955f83f.chunk.js"
+  },
+  {
+    "revision": "a16d900f74f6c0520582",
+    "url": "/MyBookstore/static/js/runtime-main.ff088815.js"
+  }
+]);
